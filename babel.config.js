4e8c6c0bb262fff8bebceed2fd4d2d44fda6363f
Gilles Manzato <1064904+GManzato@users.noreply.github.com>
2024-01-09 15:46:32 +0800

    Add babel-plugin-styled-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
+const gluestackStyleResolver = require('@gluestack-style/babel-plugin-styled-resolver');
+
+
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // plugins : ['@gluestack-style/babel-plugin-styled-resolver']
+    plugins : [gluestackStyleResolver]
   };
 };
